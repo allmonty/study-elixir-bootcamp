615a7f8f8a0bff9be0df14a2590ee1c51f6c9223
Allan David <allanbrados@gmail.com>
2018-01-17 12:41:42 -0200

    Adds doc tests

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,8 +1,11 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+    Provides methods for creating and handling a deck of cards
   """
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -17,12 +20,36 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a givern card
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, elem) do
     Enum.member?(deck, elem)
   end
 
+  @doc """
+    Divides a deck into a hando and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 
   def save(deck, filename) do
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,7 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "the answer" do
+    assert 21 + 21 == 42
   end
 end
